23e05b163bd422dd29a3f873bb043c017bb0b2ed
Brian Marick <marick@exampler.com>
2011-07-06 11:17:07 -0500

    Obey convention of using :eval-in-leiningen true

--- project.clj
+++ project.clj
@@ -1,4 +1,5 @@
 (defproject lein-midje "1.0.3"
   :author "Brian Marick"
   :description "Run Midje and clojure.test tests"
-  :url "https://github.com/marick/lein-midje")
+  :url "https://github.com/marick/lein-midje"
+  :eval-in-leiningen true)
