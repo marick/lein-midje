b999a8b63ed1cd2d9d033d0155047e1a9fe549fb
Brian Marick <marick@exampler.com>
2011-12-13 10:45:18 -0600

    Bump dev dependency on Midje to 1.3.0

--- project.clj
+++ project.clj
@@ -3,4 +3,4 @@
   :description "Run Midje and clojure.test tests"
   :url "https://github.com/marick/lein-midje"
   :eval-in-leiningen true
-  :dev-dependencies [[midje "1.3.0-RC2"]])
+  :dev-dependencies [[midje "1.3.0"]])
