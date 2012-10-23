f500a6c8e67f97cc935df5bf080aaf5f761f93d1
Brian Marick <marick@exampler.com>
2012-10-23 17:26:34 -0500

    Bump to 2.0.0 and use lein2

--- project.clj
+++ project.clj
@@ -1,8 +1,11 @@
-(defproject lein-midje/lein-midje "2.0.0-SNAPSHOT"
+(defproject lein-midje/lein-midje "2.0.0"
   :description "Run Midje and clojure.test tests"
   :url "https://github.com/marick/lein-midje"
   :author "See README"
   :eval-in-leiningen true
   :profiles {:dev {:dependencies [[midje "1.4.0"]]}}
+  :license {:name "The MIT License (MIT)"
+            :url "http://opensource.org/licenses/mit-license.php"
+            :distribution :repo}
   :min-lein-version "2.0.0")
  
