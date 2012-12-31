1afed3a4560ef078ae8ba10a60f4c83b2ababa4a
Brian Marick <marick@exampler.com>
2012-12-31 17:11:42 -0600

    I don't see that the middleware is ever useful (any more)

--- src/lein_midje/plugin.clj
+++
@@ -1,6 +0,0 @@
-(ns lein-midje.plugin)
-
-(defn middleware [project]
-  (update-in project [:profiles :dev :dependencies]
-             (fn [deps]
-               (conj (or deps []) ['bultitude "0.2.0"]))))
