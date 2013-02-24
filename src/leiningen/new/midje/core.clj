f24b90a139fe15de8a6bb6c998de8f2b9913c87d
Brian Marick <marick@exampler.com>
2013-02-23 19:10:45 -0600

    Used a different example

--- src/leiningen/new/midje/core.clj
+++ src/leiningen/new/midje/core.clj
@@ -1,4 +1,10 @@
 (ns {{sanitized}}.core)
 
-(defn equals-five? [n]
-  (= n 5))
+;;; This is an incorrect implementation, such as might be written by
+;;; someone who was used to a Lisp in which an empty list is equal to
+;;; nil.
+(defn first-element [sequence default]
+  (if (nil? sequence)
+    default
+    (first sequence)))
+
--- src/leiningen/new/midje/t_core.clj
+++ src/leiningen/new/midje/t_core.clj
@@ -2,9 +2,14 @@
   (:use midje.sweet)
   (:use [{{sanitized}}.core]))
 
-;;; Note: this fact will not check out.
-(fact "There exist numbers such that (+ n n) is equal to (* n n)."
-  (+ 0 0) => (* 0 0)
-  (+ 2 2) => (* 2 2)
-  (+ 4 4) => (* 4 4))
+(facts "about `first-element`"
+  (fact "it normally returns the first element"
+    (first-element [1 2 3] :default) => 1
+    (first-element '(1 2 3) :default) => 1)
 
+  ;; I'm a little unsure how Clojure types map onto the Lisp I'm used to.
+  (fact "default value is returned for empty sequences"
+    (first-element [] :default) => :default
+    (first-element '() :default) => :default
+    (first-element nil :default) => :default
+    (first-element (filter even? [1 3 5]) :default) => :default))
