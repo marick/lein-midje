da3615c3a1d5036d786746bc1a2c06c043f2ca4d
Brian Marick <marick@exampler.com>
2013-07-21 15:10:55 -0500

    REALLY bump version

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-midje/lein-midje "3.0.2"
+(defproject lein-midje/lein-midje "3.1.0"
   :description "Run Midje and clojure.test tests"
   :url "https://github.com/marick/lein-midje"
   :author "See README"
