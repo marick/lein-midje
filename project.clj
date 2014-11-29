5b09e9095286f27d41b5141b284e7aceb3b9efd0
Brian Marick <marick@exampler.com>
2014-11-29 15:10:06 -0600

    Bump to 3.1.4-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,9 +1,9 @@
-(defproject lein-midje/lein-midje "3.1.3"
+(defproject lein-midje/lein-midje "3.1.4-SNAPSHOT"
   :description "Run Midje and clojure.test tests"
   :url "https://github.com/marick/lein-midje"
   :author "See README"
   :eval-in-leiningen true
-  :profiles {:dev {:dependencies [[midje "1.6.0"]]}}
+  :profiles {:dev {:dependencies [[midje "1.6.3"]]}}
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
