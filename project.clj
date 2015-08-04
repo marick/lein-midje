fca33bee84cf046b5ab709b26ebe43aaf6c9a7ed
Brian Marick <marick@exampler.com>
2015-08-04 16:21:57 -0500

    tweaked project.clj

--- project.clj
+++ project.clj
@@ -2,10 +2,13 @@
   :description "Run Midje and clojure.test tests"
   :url "https://github.com/marick/lein-midje"
   :author "See README"
+  :pedantic? :warn
   :eval-in-leiningen true
   :profiles {:dev {:dependencies [[midje "1.7.0"]]}}
   :license {:name "The MIT License (MIT)"
             :url "http://opensource.org/licenses/mit-license.php"
             :distribution :repo}
-  :min-lein-version "2.0.0")
+  :min-lein-version "2.0.0"
+  :deploy-repositories [["releases" :clojars]])
+
  
