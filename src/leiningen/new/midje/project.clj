aa1b8fb3da5f3295512b31d2a81aaaf587fcd22c
Brian Marick <marick@exampler.com>
2015-08-05 10:43:07 -0500

    Bump dependencies; describe `:midje`

--- src/leiningen/new/midje/project.clj
+++ src/leiningen/new/midje/project.clj
@@ -1,5 +1,11 @@
 (defproject {{name}} "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
-  :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
+  :dependencies [[org.clojure/clojure "1.7.0"]]
+  :profiles {:dev {:dependencies [[midje "1.7.0"]]}
+             ;; You can add dependencies that apply to `lein midje` below.
+             ;; An example would be changing the logging destination for test runs.
+             :midje {}})
+             ;; Note that Midje itself is in the `dev` profile to support
+             ;; running autotest in the repl.
+
   
