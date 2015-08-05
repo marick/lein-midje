2ddfde8617f82dacf3265853e56d5b114d0f7553
Brian Marick <marick@exampler.com>
2015-08-05 11:37:19 -0500

    3.2-RC2

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject lein-midje/lein-midje "3.2-RC1"
+(defproject lein-midje/lein-midje "3.2-RC2"
   :description "Run Midje and clojure.test tests"
   :url "https://github.com/marick/lein-midje"
   :author "See README"
